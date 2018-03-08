1307ef30a6a0d227be62fcf1bc39ecf1885fa672
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2018-03-08 13:32:11 +0000

    Updated config for TTL

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,14 @@ openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout vault-ui.key
 cat <<EOF > /app/config.hcl
 listener "tcp" {
   address       = ":8000"
-  tls_cert_file    = "/app/vault-ui.crt"
-  tls_key_file    = "/app/vault-ui.key"
+  tls_cert_file = "/app/vault-ui.crt"
+  tls_key_file  = "/app/vault-ui.key"
 }
+
 vault {
-  address       = "${VAULT_ADDR}"
+  address        = "${VAULT_ADDR}"
+  runtime_config = "secret/security/goldfish"
+  approle_id     = "security_goldfish"
 }
 EOF
 
